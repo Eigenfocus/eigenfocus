e178f3d906b6c0fd4a5cdd3f7d9575279698b9e8
Vinicius Oyama <contact@viniciusoyama.com>
2024-12-26 14:06:43 -0300

    feat(reports/total_time): Implements total time spent report

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,26 @@
+class ReportsController < ApplicationController
+  def total_time
+    if params[:report]
+      @time_entries = TimeEntry.includes(:project, :issue)
+                      .order(reference_date: :asc)
+
+      if params[:report][:start_at].present?
+        @time_entries = @time_entries.where("reference_date >= ?", params[:report][:start_at].to_date)
+      end
+
+      if params[:report][:end_at].present?
+        @time_entries = @time_entries.where("reference_date <= ?", params[:report][:end_at].to_date)
+      end
+
+      project_ids = (params[:report][:project_ids] || []).reject(&:blank?)
+
+      if project_ids.any?
+        @time_entries = @time_entries.where(project_id: project_ids)
+      end
+
+      @total_in_hours = (@time_entries.sum(:total_logged_time_in_minutes)/60.0).round(2)
+    else
+      params[:report] = {}
+    end
+  end
+end
---
+++ spec/features/reports/time_report_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+context "As a user, I want to generate time reports" do
+  around(:example) do |ex|
+    Timecop.freeze("2024-07-13 12:00".to_datetime) do
+      ex.run
+    end
+  end
+
+  let!(:user) { FactoryBot.create(:user) }
+
+  let(:project_alpha) { FactoryBot.create(:project, name: 'Alpha') }
+  let(:project_beta) { FactoryBot.create(:project, name: 'Beta') }
+
+  before(:each) do
+    FactoryBot.create(:time_entry, user:, project: project_alpha, total_logged_time_in_minutes: 10, reference_date: 7.days.ago)
+    FactoryBot.create(:time_entry, user:, project: project_alpha, total_logged_time_in_minutes: 15, reference_date: 6.days.ago)
+
+    FactoryBot.create(:time_entry, user:, project: project_beta, total_logged_time_in_minutes: 90, reference_date: 2.days.ago)
+    FactoryBot.create(:time_entry, user:, project: project_beta, total_logged_time_in_minutes: 120, reference_date: 1.days.ago)
+  end
+
+  specify "I can filter by projects" do
+    visit total_time_reports_path
+
+    click_button "Generate report"
+
+    within 'form' do
+      select "Alpha", from: "report_project_ids"
+      click_button "Generate report"
+    end
+
+    within '#report' do
+      expect(page).to have_content("0.42 hours")
+    end
+
+    within 'form' do
+      select "Beta", from: "report_project_ids"
+      click_button "Generate report"
+    end
+
+    within '#report' do
+      expect(page).to have_content("3.92 hours") # alpha + beta
+    end
+
+    expect(all("table tbody tr").count).to eq(4)
+  end
+
+  specify "I can filter by time period" do
+    visit total_time_reports_path
+
+    within 'form' do
+      fill_in :report_start_at, with: "2024-07-12" # one day ago
+      click_button "Generate report"
+    end
+
+    within '#report' do
+      expect(page).to have_content("2.0 hours") # alpha + beta
+    end
+
+    within 'form' do
+      fill_in :report_start_at, with: "2024-07-7" # 6 days ago
+      fill_in :report_end_at, with: "2024-07-11" # 2 days ago ago
+      page.execute_script("document.getElementById('report_end_at').blur()")
+      click_button "Generate report"
+    end
+
+    within '#report' do
+      expect(page).to have_content("1.75 hours") # alpha + beta
+    end
+  end
+end
