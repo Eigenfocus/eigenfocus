ca77aa5e26598e7b36e92d5481385e5ef5799b77
Vinicius Oyama <contact@viniciusoyama.com>
2024-12-26 17:36:30 -0300

    Create sortable controller to handle sortable elements on UI

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from 'sortablejs'
+import { FetchRequest } from '@rails/request.js'
+
+export default class extends Controller {
+	static targets = ['container']
+	static values = {
+		movePath: String
+	}
+
+	connect() {
+		this.sortable = new Sortable(this.containerTarget, {
+			animation: 150,
+			onEnd: this.onDragEnd.bind(this)
+		})
+	}
+
+	onDragEnd(evt) {
+		const request = new FetchRequest('post', this.movePathValue, {
+			body: JSON.stringify({
+				position: {
+					from: evt.oldIndex,
+					to: evt.newIndex
+				}
+			})
+		})
+
+		request.perform()
+	}
+}
