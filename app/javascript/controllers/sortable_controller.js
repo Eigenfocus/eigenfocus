77d3f01c917d5d62bddcac223e9c10d38b475a48
Vinicius Oyama <contact@viniciusoyama.com>
2024-12-27 10:42:58 -0300

    Fix js indentation with space

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -3,28 +3,28 @@ import Sortable from 'sortablejs'
 import { FetchRequest } from '@rails/request.js'
 
 export default class extends Controller {
-	static targets = ['container']
-	static values = {
-		movePath: String
-	}
+  static targets = ['container']
+  static values = {
+    movePath: String
+  }
 
-	connect() {
-		this.sortable = new Sortable(this.containerTarget, {
-			animation: 150,
-			onEnd: this.onDragEnd.bind(this)
-		})
-	}
+  connect() {
+    this.sortable = new Sortable(this.containerTarget, {
+      animation: 150,
+      onEnd: this.onDragEnd.bind(this)
+    })
+  }
 
-	onDragEnd(evt) {
-		const request = new FetchRequest('post', this.movePathValue, {
-			body: JSON.stringify({
-				position: {
-					from: evt.oldIndex,
-					to: evt.newIndex
-				}
-			})
-		})
+  onDragEnd(evt) {
+    const request = new FetchRequest('post', this.movePathValue, {
+      body: JSON.stringify({
+        position: {
+          from: evt.oldIndex,
+          to: evt.newIndex
+        }
+      })
+    })
 
-		request.perform()
-	}
+    request.perform()
+  }
 }
