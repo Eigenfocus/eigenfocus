3479b9328f2c4d0606c83f2af7745c6eef7be457
Vinicius Oyama <contact@viniciusoyama.com>
2024-12-27 17:19:06 -0300

    Change JS sortable controller to work with multiple groups

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -5,12 +5,14 @@ import { FetchRequest } from '@rails/request.js'
 export default class extends Controller {
 	static targets = ['container']
 	static values = {
-		movePath: String
+		movePath: String,
+		group: { type: String, default: null }
 	}
 
 	connect() {
 		this.sortable = new Sortable(this.containerTarget, {
 			animation: 150,
+			group: this.groupValue,
 			onEnd: this.onDragEnd.bind(this)
 		})
 	}
@@ -18,9 +20,13 @@ export default class extends Controller {
 	onDragEnd(evt) {
 		const request = new FetchRequest('post', this.movePathValue, {
 			body: JSON.stringify({
-				position: {
-					from: evt.oldIndex,
-					to: evt.newIndex
+				from_position: {
+					group_id: evt.from.dataset.sortableGroupIdValue,
+					index: evt.oldIndex
+				},
+				to_position: {
+					group_id: evt.to.dataset.sortableGroupIdValue,
+					index: evt.newIndex
 				}
 			})
 		})
