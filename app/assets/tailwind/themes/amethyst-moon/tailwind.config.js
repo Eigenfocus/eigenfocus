6627b1c41709f27d55fe862a8d8bc7a9c0b5a216
Vinicius Oyama <contact@viniciusoyama.com>
2025-01-02 11:18:18 -0300

    ref(themes): extracts shared config to a single file

--- app/assets/tailwind/themes/amethyst-moon/tailwind.config.js
+++ app/assets/tailwind/themes/amethyst-moon/tailwind.config.js
@@ -1,6 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
 const tailwindColors = require('tailwindcss/colors')
-const plugin = require('tailwindcss/plugin')
+const { generateConfig } = require('../default.config')
 
 const themeColors = {
   primary: tailwindColors.indigo,
@@ -19,50 +18,4 @@ const themeColors = {
   "body-contrast": '#fff'
 }
 
-module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  safelist: [
-    ...(['50','100','200','300','400','500','600','700','800']
-      .flatMap(number => Object.keys(themeColors).map( (colorName) => `${colorName}-${number}`) )
-      .flatMap(color => [`bg-${color}`, `text-${color}`, `border-${color}`])
-      .flatMap(variant => [variant, `hover:${variant}`, `focus:${variant}`, `active:${variant}`, `placeholder:${variant}`])),
-    "border-r-2", // For menu
-    "font-thin","font-extralight","font-light","font-normal","font-medium","font-semibold","font-bold","font-extrabold","font-black",
-    "w-full", "h-full" // For modal
-  ],
-  darkMode: "class",
-  theme: {
-    container: {
-      center: true,
-      padding: "1rem",
-    },
-    colors: themeColors,
-    extend: {
-      zIndex: {
-        999999: '999999',
-        99999: '99999',
-        9999: '9999',
-        999: '999',
-        99: '99',
-        9: '9',
-        1: '1',
-      },
-    }
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    plugin(({ addVariant, e }) => {
-      addVariant('sidebar-expanded', ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => `.sidebar-expanded .${e(`sidebar-expanded${separator}${className}`)}`);
-      });
-    })
-  ]
-}
+module.exports = generateConfig(themeColors)
