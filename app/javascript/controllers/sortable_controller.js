46a8e3cd6b1830028407f30f352936fb19a37c97
Vinícius Oyama <contact@viniciusoyama.com>
2025-01-03 10:03:34 -0500

    feat(boards): improves column feedback for dragging card (#27)
    * Improve column feedback for card dragging
    
    * Clean up code

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -2,25 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 import Sortable from 'sortablejs'
 import { FetchRequest } from '@rails/request.js'
 
+const SELECTED_CONTAINER_CSS_CLASS = 'sortable-selected-container'
+
 export default class extends Controller {
   static targets = ['container']
   static values = {
     movePath: String,
     sharedGroup: String,
     groupingId: String, // Not used by stimulus directly
-    ignoreDragSelector: String
+    ignoreDragSelector: String,
+    ancestorContainerSelector: String
   }
 
   connect() {
     this.sortable = new Sortable(this.containerTarget, {
       animation: 150,
+      emptyInsertThreshold: 30,
       group: this.sharedGroupValue,
       filter: this.ignoreDragSelectorValue,
-      onEnd: this.onDragEnd.bind(this)
+      onEnd: this.onDragEnd.bind(this),
+      onMove: this.onMove.bind(this)
     })
   }
 
   onDragEnd(evt) {
+    this._clearSelectedContainer()
+
+    let didGroupingChanged = evt.from.dataset.sortableGroupingId !== evt.to.dataset.sortableGroupingId
+    let didPositionChanged = evt.oldIndex !== evt.newIndex
+
+    let didItemReallyMove = didGroupingChanged || didPositionChanged
+
+    if (didItemReallyMove) {
+      this._performMoveRequest(evt)
+    }
+  }
+
+  _performMoveRequest(evt) {
     const request = new FetchRequest('post', this.movePathValue, {
       body: JSON.stringify({
         from: {
@@ -35,5 +53,24 @@ export default class extends Controller {
     })
 
     request.perform()
+  }
+
+  onMove(evt) {
+    if (this.ancestorContainerSelectorValue) {
+      this._clearSelectedContainer()
+
+      this.container = evt.to.closest(this.ancestorContainerSelectorValue)
+
+      if (this.container) {
+        this.container.classList.add(SELECTED_CONTAINER_CSS_CLASS)
+      }
+    }
+  }
+
+  _clearSelectedContainer() {
+    if (this.container) {
+      this.container.classList.remove(SELECTED_CONTAINER_CSS_CLASS)
+      this.container = null
+    }
   }
 }
