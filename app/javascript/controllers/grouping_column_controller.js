7de9fb7daa7678cd77030240282b4021cb7dd2cc
Vinícius Oyama <contact@viniciusoyama.com>
2025-01-08 12:30:50 -0300

    Inline form cards and button styling

--- app/javascript/controllers/grouping_column_controller.js
+++ app/javascript/controllers/grouping_column_controller.js
@@ -4,15 +4,18 @@ import { marked } from "marked";
 export default class extends Controller {
   static targets = [
     "card",
+    "showFormButton",
     "inlineCardForm"
   ]
 
   showInlineCardForm() {
     this.inlineCardFormTarget.classList.remove('hidden')
+    this.showFormButtonTarget.classList.add('hidden')
     this.inlineCardFormTarget.querySelector("input[name='issue[title]'").focus()
   }
 
   hideInlineCardForm() {
+    this.showFormButtonTarget.classList.remove('hidden')
     this.inlineCardFormTarget.classList.add('hidden')
   }
 }
