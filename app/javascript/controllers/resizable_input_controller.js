2a18f7848536b176ac488dcbb35f94d378de958a
Vinícius Oyama <contact@viniciusoyama.com>
2025-01-09 12:10:55 -0300

    Card title input using resizable input

---
+++ app/javascript/controllers/resizable_input_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Based on https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/
+export default class extends Controller {
+  static targets = [
+    "replica",
+    "input"
+  ]
+
+  connect() {
+    this.replicaTarget.innerHTML = this.inputTarget.value
+
+    if (this.inputTarget.value.trim() == '') {
+      this.replicaTarget.innerHTML += '&nbsp;' // Avoid wrong height if input is empty
+    }
+
+    this.inputTarget.addEventListener('input', () => {
+      console.log(this.inputTarget.value)
+      this.replicaTarget.innerText = this.inputTarget.value + '&nbsp;' // the ' ' avoid ui jumps
+    })
+  }
+}
