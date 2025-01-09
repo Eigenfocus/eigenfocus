ff0e16e13defdeeb621a5ce49e9d3ce2ff36dbce
Vinícius Oyama <contact@viniciusoyama.com>
2025-01-09 15:31:46 -0300

    chore(select2): autofocus search input after opening select options

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -15,6 +15,15 @@ export default class extends Controller {
       this.dispatchEvent(event);
     });
 
+    this.select2.on('select2:open', function () {
+      window.setTimeout(function () {
+        // wait for UI render to show the input
+        if (document.querySelector('input.select2-search__field')) {
+          document.querySelector('input.select2-search__field').focus();
+        }
+      }, 0);
+    });
+
     if (!window.__select2binded__) {
       $(document).on('focus', '.select2-selection.select2-selection--single', function (e) {
         $(this).closest(".select2-container").siblings('select:enabled').select2('open');
