20a445a981a1e2badda2a10b05258a25366fc08e
Vinícius Oyama <contact@viniciusoyama.com>
2025-01-11 14:57:08 -0300

    chore: nullifying issue_id on TimeEntry when an Issue is destroyed

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,7 @@ class Issue < ApplicationRecord
   # Relations
   belongs_to :project
   has_many_attached :files
+  has_many :time_entries, dependent: :nullify
 
   # Validations
   validates :title, presence: true
