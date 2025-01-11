e6a7f5fdd7c909354afc597f6935d0c1902baab6
Vinícius Oyama <contact@viniciusoyama.com>
2025-01-11 15:58:00 -0300

    chore(Board): enables auto scroll while dragging

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -16,6 +16,7 @@ export default class extends Controller {
 
   connect() {
     this.sortable = new Sortable(this.containerTarget, {
+      scroll: true, // Enable the plugin. Can be HTMLElement
       animation: 150,
       emptyInsertThreshold: 30,
       group: this.sharedGroupValue,
