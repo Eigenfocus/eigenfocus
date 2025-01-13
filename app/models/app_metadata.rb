4bd7d2f230efb581ceb8f812aae2dc2381571590
Vinícius Oyama <contact@viniciusoyama.com>
2025-01-13 09:15:55 -0300

    Setting default values for AppMetadata when creating singleton instance

--- app/models/app_metadata.rb
+++ app/models/app_metadata.rb
@@ -1,5 +1,6 @@
 class AppMetadata < ApplicationRecord
-  before_create :assign_token
+  # Hooks
+  before_create :assign_fields
 
   def self.instance
     first_or_create
@@ -9,7 +10,13 @@ class AppMetadata < ApplicationRecord
     Gem::Version.new(File.read(Rails.root.join("VERSION")))
   end
 
-  private def assign_token
+  def last_released_version
+    Gem::Version.new(super)
+  end
+
+  private def assign_fields
     self.token = SecureRandom.uuid
+    self.last_released_version = current_version
+    self.last_released_version_updated_at = DateTime.current
   end
 end
--- spec/models/app_metadata_spec.rb
+++ spec/models/app_metadata_spec.rb
@@ -3,20 +3,42 @@ require 'rails_helper'
 describe AppMetadata do
   subject { AppMetadata.instance }
 
-  it "is a singleton db entry" do
-    expect(AppMetadata.count).to be(0)
-    app_metadata1 = AppMetadata.instance
-    expect(AppMetadata.count).to be(1)
-    app_metadata2 = AppMetadata.instance
-    expect(AppMetadata.count).to be(1)
-    expect(app_metadata1).to eq(app_metadata2)
+  describe '.instance' do
+    it "is a singleton db entry" do
+      expect(AppMetadata.count).to be(0)
+      app_metadata1 = AppMetadata.instance
+      expect(app_metadata1).to be_persisted
+
+      expect(AppMetadata.count).to be(1)
+      app_metadata2 = AppMetadata.instance
+      expect(AppMetadata.count).to be(1)
+      expect(app_metadata1).to eq(app_metadata2)
+    end
+  end
+
+  describe 'singleton setup' do
+    it "sets last_released_version to current_version when creating" do
+      expect(subject.last_released_version).to eq(subject.current_version)
+    end
+
+    it 'sets a token' do
+      expect(subject.token).to match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/)
+    end
+
+    it "sets last_released_version_updated_at to now" do
+      current_time = DateTime.current
+
+      Timecop.freeze(current_time) do
+        expect(subject.last_released_version_updated_at.utc.to_s).to eq(current_time.utc.to_s)
+      end
+    end
   end
 
   specify "#current_version" do
     expect(subject.current_version).to be_an_instance_of(Gem::Version)
   end
 
-  specify '#token' do
-    expect(subject.token).to match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/)
+  specify '#last_released_version' do
+    expect(subject.last_released_version).to be_an_instance_of(Gem::Version)
   end
 end
