f8f4e7d618a2909a471d58efa732388165dfa6e4
Vinícius Oyama <contact@viniciusoyama.com>
2025-01-19 15:26:07 -0300

    Adds create column to main board view menu

--- app/controllers/visualizations_controller.rb
+++ app/controllers/visualizations_controller.rb
@@ -1,5 +1,6 @@
 class VisualizationsController < ApplicationController
   def show
     @visualization = Visualization.includes(groupings: :issues).find(params[:id])
+    skip_layout_content_wrapper!
   end
 end
