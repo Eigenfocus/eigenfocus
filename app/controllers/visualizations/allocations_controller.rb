6638776d2cd7c94772633c99e10d794a53c826cf
Jun <felipe.jun.ohira@gmail.com>
2025-02-04 18:18:44 -0300

    Add live update to allocations changes

--- app/controllers/visualizations/allocations_controller.rb
+++ app/controllers/visualizations/allocations_controller.rb
@@ -1,13 +1,23 @@
 class Visualizations::AllocationsController < ApplicationController
   def move
     @allocation = GroupingIssueAllocation.find_by(grouping_id: move_params[:from][:group], position: move_params[:from][:position])
+    from_grouping = @allocation.grouping
     @allocation.update(grouping_id: move_params[:to][:group], position: move_params[:to][:position])
     @grouping = @allocation.grouping
     @issue = @allocation.issue
+
+    broadcast_move(from_grouping, params)
   end
 
   private
   def move_params
     params.deep_transform_keys(&:to_sym)
+  end
+
+  def broadcast_move(grouping, params)
+    Visualizations::AllocationsChannel.broadcast_move(
+      grouping.id,
+      params.permit(:origin, from: [ :position, :group ], to: [ :position, :group ])
+    )
   end
 end
