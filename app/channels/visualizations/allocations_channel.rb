f744d7c02ec8ad4822d7bfe3250f4f36b0ed6e9d
Jun <felipe.jun.ohira@gmail.com>
2025-02-04 18:18:44 -0300

    Add live update to allocations changes

---
+++ app/channels/visualizations/allocations_channel.rb
@@ -0,0 +1,13 @@
+class Visualizations::AllocationsChannel < ApplicationCable::Channel
+  def self.broadcast_move(grouping_id, move)
+    ActionCable.server.broadcast("visualizations/#{grouping_id}/allocations/move", move)
+  end
+
+  def subscribed
+    stream_from "visualizations/#{params[:grouping_id]}/allocations/move"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
