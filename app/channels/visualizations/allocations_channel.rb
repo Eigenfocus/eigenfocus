937c3ed3ffc659407e6e5c89d84a9e3d1abcd222
Jun <felipe.jun.ohira@gmail.com>
2025-02-04 22:23:27 -0300

    Change channel stream to be identified by visualization instead of grouping

--- app/channels/visualizations/allocations_channel.rb
+++ app/channels/visualizations/allocations_channel.rb
@@ -1,9 +1,9 @@
 class Visualizations::AllocationsChannel < ApplicationCable::Channel
-  def self.broadcast_move(grouping_id, move)
-    ActionCable.server.broadcast("visualizations/#{grouping_id}/allocations/move", move)
+  def self.broadcast_move(visualization_id, move)
+    ActionCable.server.broadcast("visualizations/#{visualization_id}/allocations/move", move)
   end
 
   def subscribed
-    stream_from "visualizations/#{params[:grouping_id]}/allocations/move"
+    stream_from "visualizations/#{params[:visualization_id]}/allocations/move"
   end
 end
