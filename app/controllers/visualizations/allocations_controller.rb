82909575a80a9332257f09d8c206241131fa3275
Jun <felipe.jun.ohira@gmail.com>
2025-02-04 22:23:27 -0300

    Change channel stream to be identified by visualization instead of grouping

--- app/controllers/visualizations/allocations_controller.rb
+++ app/controllers/visualizations/allocations_controller.rb
@@ -1,12 +1,11 @@
 class Visualizations::AllocationsController < ApplicationController
   def move
     @allocation = GroupingIssueAllocation.find_by(grouping_id: move_params[:from][:group], position: move_params[:from][:position])
-    from_grouping = @allocation.grouping
     @allocation.update(grouping_id: move_params[:to][:group], position: move_params[:to][:position])
     @grouping = @allocation.grouping
     @issue = @allocation.issue
 
-    broadcast_move(from_grouping, params)
+    broadcast_move(@allocation.grouping.visualization, params)
   end
 
   private
@@ -14,9 +13,9 @@ class Visualizations::AllocationsController < ApplicationController
     params.deep_transform_keys(&:to_sym)
   end
 
-  def broadcast_move(grouping, params)
+  def broadcast_move(visualization, params)
     Visualizations::AllocationsChannel.broadcast_move(
-      grouping.id,
+      visualization.id,
       params.permit(:origin, from: [ :position, :group ], to: [ :position, :group ])
     )
   end
