ff9ec2cc7d33f4a69a337abec48e561ab0cd9938
Vinícius Oyama <contact@viniciusoyama.com>
2025-02-06 20:58:46 -0300

    Formats labels on boards

--- app/helpers/application_helper/labels.rb
+++ app/helpers/application_helper/labels.rb
@@ -2,15 +2,13 @@ module ApplicationHelper
   module Labels
     def badge_for_label(label)
       options = {
-        class: "inline-flex rounded-full border-#{color_for_label(label)} text-#{color_for_label(label)} border py-1 px-2 text-xs font-medium"
+        class: "inline-flex rounded-full border-readable-content-400 text-readable-content-400 border border py-1 px-2 text-xs font-medium",
+        style: ""
       }
 
       content_tag(:span, options) do
         label.title
       end
-    end
-
-    def color_for_label(label)
     end
   end
 end
