ef121d5f39a4c42cc812c6d62b00b140ed8781ad
Vinícius Oyama <contact@viniciusoyama.com>
2025-02-07 10:20:08 -0300

    Live updating card labels

--- app/models/issue_label_link.rb
+++ app/models/issue_label_link.rb
@@ -1,5 +1,5 @@
 class IssueLabelLink < ApplicationRecord
-  belongs_to :issue
+  belongs_to :issue, touch: true
   belongs_to :issue_label
 
   validates :issue_label_id, uniqueness: { scope: :issue_id }
