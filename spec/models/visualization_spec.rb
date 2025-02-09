b857790903d98e3e6a3490be5ad2e818f2a7fa09
Vinícius Oyama <contact@viniciusoyama.com>
2025-02-09 12:39:19 -0300

    Removes unused spec for visualization favorite issues array

--- spec/models/visualization_spec.rb
+++ spec/models/visualization_spec.rb
@@ -1,13 +1,4 @@
 require 'rails_helper'
 
 describe Visualization do
-  describe '#favorite_issue_labels' do
-    it "returns at least 6 elements" do
-      visualization = FactoryBot.create :visualization
-      visualization.favorite_issue_labels = []
-      visualization.save!
-
-      expect(visualization.favorite_issue_labels).to eq (Array.new(6))
-    end
-  end
 end
