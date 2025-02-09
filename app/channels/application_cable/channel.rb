30aa7dd40bf628af8909dacec99d35d79d970e06
Vinícius Oyama <contact@viniciusoyama.com>
2025-02-09 08:49:25 -0300

    Changes ActionCable to verify signed params

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    before_subscribe :verify_token
+    attribute_reader :signed_params
+
+    def verify_token
+      return unless params[:token].present?
+
+      begin
+        @signed_params = HashWithIndifferentAccess.new(StreamSigner.verify_token(params[:token]))
+        puts signed_params
+      rescue
+        reject_unauthorized_connection
+      end
+    end
   end
 end
