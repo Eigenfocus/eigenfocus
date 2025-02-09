e6de50074f7acbedb3f8e9219035de9514403022
Vinícius Oyama <contact@viniciusoyama.com>
2025-02-09 11:22:24 -0300

    Implements adding a issue label via shortcut

--- app/javascript/controllers/visualization/favorite_issue_labels_controller.js
+++ app/javascript/controllers/visualization/favorite_issue_labels_controller.js
@@ -1,5 +1,5 @@
-import { Controller } from "@hotwired/stimulus"
-import consumer from "channels/consumer"
+import { Controller } from '@hotwired/stimulus'
+import { FetchRequest } from '@rails/request.js'
 
 const ISSUE_DATA_ATTRIBUTE = 'data-favorite-issue-labels-issue-id'
 
@@ -7,6 +7,8 @@ export default class extends Controller {
   static values = {
     addPath: String,
   }
+
+  static targets = ['favoriteLabelInput']
 
   onKeyPress(e) {
     const key = e.key
@@ -17,10 +19,29 @@ export default class extends Controller {
       if (hoveredCard) {
         e.preventDefault()
         const issueId = hoveredCard.getAttribute(ISSUE_DATA_ATTRIBUTE)
-        console.log(`Label ${key} applied ${issueId}`)
-        console.log(this.addPathValue)
+        const label = this.favoriteLabelInputTargets[parseInt(key) - 1].value
+
+        this.#applyLabel(issueId, label)
       }
     }
+  }
+
+  #applyLabel(issueId, label) {
+    if (label.trim() === "") {
+      return;
+    }
+
+    console.log(`Label ${label} applied ${issueId}`)
+    console.log(this.addPathValue)
+    const REQUEST_PATH = this.addPathValue.replace("ISSUE_ID_PLACEHOLDER", issueId)
+
+    const request = new FetchRequest('post', REQUEST_PATH, {
+      body: JSON.stringify({
+        label: { title: label }
+      })
+    })
+
+    return request.perform()
   }
 
 }
