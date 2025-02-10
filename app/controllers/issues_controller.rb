f5a1f82009d047662240eaeb79dc1f9e571accc7
Jun <felipe.jun.ohira@gmail.com>
2025-02-10 17:00:30 -0300

    Add grouping picker on issue details

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,4 +1,17 @@
 class IssuesController < ApplicationController
+  def pick_grouping
+    issue = Issue.find(params[:id])
+    grouping = issue.project.default_visualization.groupings.find_by(id: params[:grouping_id])
+
+    if grouping.present?
+      @allocation = issue.grouping_issue_allocations.first_or_initialize
+      @allocation.update(grouping: grouping, position: :last)
+    else
+      @allocation = issue.grouping_issue_allocations.first_or_initialize
+      @allocation.delete
+    end
+  end
+
   def destroy
     @issue = Issue.find(params[:id])
     @issue.destroy
