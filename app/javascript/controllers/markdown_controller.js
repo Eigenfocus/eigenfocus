61f967fd92a9b73ef8c511a0b7b92bb6071e99c5
Vinícius Oyama <contact@viniciusoyama.com>
2025-02-10 17:26:58 -0300

    ref: new issue modal from all issues view uses a different form

---
+++ app/javascript/controllers/markdown_controller.js
@@ -0,0 +1,51 @@
+import { Controller } from "@hotwired/stimulus";
+import { marked } from "marked";
+
+export default class extends Controller {
+  static targets = [
+    "descriptionPreview",
+    "descriptionInput",
+    "showEditorButton",
+    "showPreviewButton"
+  ]
+
+  connect() {
+    this._simplemde = new SimpleMDE({
+      element: this.descriptionInputTarget,
+      spellChecker: false,
+      showIcons: [
+        "code",
+        "table"
+
+      ],
+      hideIcons: [
+        "preview",
+        "fullscreen",
+        "side-by-side",
+        "guide"
+      ],
+      previewRender: (plainText, preview) => {
+        window.test = marked.parse(plainText)
+        return marked.parse(plainText);
+      },
+      status: []
+    });
+
+    this._simplemde.codemirror.on("change", () => {
+      this.descriptionInputTarget.value = this._simplemde.value();
+      this.descriptionPreviewTarget.innerHTML = marked.parse(this._simplemde.value());
+    });
+  }
+
+  enablePreview() {
+    this._simplemde.togglePreview();
+    this.showPreviewButtonTarget.classList.add("hidden")
+    this.showEditorButtonTarget.classList.remove("hidden")
+  }
+
+  disablePreview() {
+    this._simplemde.togglePreview();
+    this.showPreviewButtonTarget.classList.remove("hidden")
+    this.showEditorButtonTarget.classList.add("hidden")
+  }
+}
