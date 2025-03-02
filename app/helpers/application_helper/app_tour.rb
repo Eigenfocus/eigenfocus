fc52818434fd7c1df680e562c3812d8d0358a220
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-02 16:58:52 -0300

    ref: implements app_tour_tags helper

--- app/helpers/application_helper/app_tour.rb
+++ app/helpers/application_helper/app_tour.rb
@@ -1,14 +1,15 @@
 module ApplicationHelper
   module AppTour
-    def activate_tour_tag(tour_key)
-      javascript_tag <<~JS
-        document.addEventListener("turbo:load", () => {
-          window.appTour.start("#{tour_key}");
-        }, { once: true });
-      JS
+    def app_tour_tags
+      capture do
+        output = ""
+        output += start_app_tour_tag(params[:activate_tour]) if params[:activate_tour].present?
+        output += mark_app_tours_as_pending_tag if params[:mark_app_tours_as_pending] == "true"
+        output.html_safe
+      end
     end
 
-    def show_app_tour_for(tour_key = nil)
+    def start_pending_app_tour_tag(tour_key = nil)
       javascript_tag <<~JS
         document.addEventListener("turbo:load", () => {
           if (window.appTour) {
@@ -17,15 +18,23 @@ module ApplicationHelper
         }, { once: true });
       JS
     end
-  end
 
-  def mark_app_tours_as_pending
-    javascript_tag <<~JS
-      document.addEventListener("turbo:load", () => {
-        if (window.appTour) {
-          window.appTour.markAllToursAsPending();
-        }
-      }, { once: true });
-    JS
+    def start_app_tour_tag(tour_key)
+      javascript_tag <<~JS
+        document.addEventListener("turbo:load", () => {
+          window.appTour.start("#{tour_key}");
+        }, { once: true });
+      JS
+    end
+
+    def mark_app_tours_as_pending_tag
+      javascript_tag <<~JS
+        document.addEventListener("turbo:load", () => {
+          if (window.appTour) {
+            window.appTour.markAllToursAsPending();
+          }
+        }, { once: true });
+      JS
+    end
   end
 end
