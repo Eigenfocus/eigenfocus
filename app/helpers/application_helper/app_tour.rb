bf296c4dabaa2a3e790a68e0d21f9722672767d5
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-02 16:54:18 -0300

    Activating tour via querystring parameter

--- app/helpers/application_helper/app_tour.rb
+++ app/helpers/application_helper/app_tour.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
   module AppTour
+    def activate_tour_tag(tour_key)
+      javascript_tag <<~JS
+        document.addEventListener("turbo:load", () => {
+          window.appTour.start("#{tour_key}");
+        }, { once: true });
+      JS
+    end
+
     def show_app_tour_for(tour_key = nil)
       javascript_tag <<~JS
         document.addEventListener("turbo:load", () => {
