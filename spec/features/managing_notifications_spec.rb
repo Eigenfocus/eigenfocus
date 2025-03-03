341e4c4790a2be040e522f5fa29c37ac48fc0bc1
Jun <felipe.jun.ohira@gmail.com>
2025-03-03 11:15:33 -0300

    Add external link tests

--- spec/features/managing_notifications_spec.rb
+++ spec/features/managing_notifications_spec.rb
@@ -51,5 +51,29 @@ RSpec.describe "Managing notifications", type: :feature do
         expect(page).not_to have_button("Mark all as read")
       end
     end
+
+    context "with external link notifications" do
+      let!(:notification) { create(:notification, :with_external_link, external_id: "NEW_NOTIFICATION") }
+
+      before do
+        create_list(:notification, 2, :with_external_link)
+        visit root_path
+      end
+
+      it "shows the go to link" do
+        find(".cpy-notifications-dropdown").click
+        expect(all(".cpy-external-link").count).to eq(3)
+      end
+
+      it "shows to correct link for external link notifications" do
+        find(".cpy-notifications-dropdown").click
+
+        within dom_id(notification) do
+          link = find(".cpy-external-link")
+          expect(link["href"]).to eq("https://notifications.eigenfocus.com/NEW_NOTIFICATION")
+          expect(link["target"]).to eq("_blank")
+        end
+      end
+    end
   end
 end
