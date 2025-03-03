1b38141d592ffb0962f6b4988cb228561f434b97
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-03 14:33:53 -0300

    chore: updates self hosted api client endpoint

--- lib/app_version_updater.rb
+++ lib/app_version_updater.rb
@@ -21,22 +21,8 @@ class AppVersionUpdater
   end
 
   private def fetch_newest_version
-    return if Rails.env.development?
-    begin
-      uri = URI("https://eigenfocus.com/api/v1/app_versions/latest")
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = (uri.scheme == "https")
-
-      request = Net::HTTP::Get.new(uri)
-      request["App-Token"] = app_metadata.token
-      request["App-Version"] = app_metadata.current_version.to_s
-
-      response = http.request(request)
-      response = JSON.parse(response.body)
-      response["latest_version"]
-    rescue
-      ""
-    end
+    client = SelfHostedApiClient.new(app_metadata)
+    response = client.get("/app_versions/latest")
+    response&.fetch("latest_version", "")
   end
 end
