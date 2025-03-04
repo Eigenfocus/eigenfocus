b234b39b1a228df170d79b0621ad0792f2190158
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-04 14:42:43 -0300

    Implements survey responses async submission

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -1,4 +1,14 @@
 class SurveyResponsesController < ApplicationController
   def create
+    survey_data = {
+      team_size: params[:team_size],
+      utilization_context: params[:utilization_context],
+      features_used: params[:usage_purposes]
+    }
+
+    app_metadata = AppMetadata.instance
+    app_metadata.update!(onboarding_survey_response: survey_data)
+
+    SubmitSurveyResponseJob.perform_later
   end
 end
---
+++ app/jobs/submit_survey_response_job.rb
@@ -0,0 +1,19 @@
+class SubmitSurveyResponseJob < ApplicationJob
+  def perform
+    app_metadata = AppMetadata.instance
+    survey_response = app_metadata.onboarding_survey_response
+
+    return if survey_response.blank?
+
+    client = SelfHostedApiClient.new(app_metadata)
+
+    client.post("/v1/survey_responses/submit", {
+      responder_token: app_metadata.survey_token,
+      survey_response: {
+        team_size: survey_response["team_size"],
+        utilization_context: survey_response["utilization_context"],
+        features_used: survey_response["features_used"]
+      }
+    })
+  end
+end
