5899e9c33a455c3785e2aee3a634eef7b891609f
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-04 12:37:57 -0300

    Adds post request type to self hosted api client

--- lib/self_hosted_api_client.rb
+++ lib/self_hosted_api_client.rb
@@ -26,4 +26,24 @@ class SelfHostedApiClient
   rescue
     {}
   end
+
+  def post(path, body = {})
+    return {} if Rails.env.development?
+
+    uri = URI(BASE_URL.delete_suffix("/") + "/" + path.delete_prefix("/"))
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == "https")
+
+    request = Net::HTTP::Post.new(uri)
+    request["App-Token"] = @app_metadata.token
+    request["App-Version"] = @app_metadata.current_version.to_s
+    request["Last-Used-At"] = @app_metadata.last_used_at.iso8601 if @app_metadata.last_used_at.present?
+    request["Content-Type"] = "application/json"
+    request.body = body.to_json
+
+    response = http.request(request)
+    JSON.parse(response.body)
+  rescue
+    {}
+  end
 end
