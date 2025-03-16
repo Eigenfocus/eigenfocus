aeebdec6559989ad45e82ee2e16ec4fa609d5bed
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-16 16:39:38 -0300

    chore: keeps time entries listing behind form when opening add/edit in a new tab

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -47,7 +47,11 @@ class TimeEntriesController < ApplicationController
   def edit
     @time_entry = current_user.time_entries.find(params[:id])
 
-    render partial: "form", locals: { time_entry: @time_entry }
+    if turbo_frame_request?
+      render partial: "form", locals: { time_entry: @time_entry }
+    else
+      redirect_to time_entries_path(time_entry_id: params[:id])
+    end
   end
 
   def update
