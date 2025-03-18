d5263f4fb45693c785a9c49e5640da5391a78f41
Jun <felipe.jun.ohira@gmail.com>
2025-03-18 16:18:46 -0300

    Add monkey patch to add broadcast methods of dispatch event directly on model

--- config/initializers/turbo_stream.rb
+++ config/initializers/turbo_stream.rb
@@ -10,4 +10,43 @@ module TurboStreamDispatchEventHelper
   end
 end
 
+module Turbo::Streams::Broadcasts
+  def broadcast_dispatch_message_to(*streamables, action:, **opts)
+    broadcast_stream_to(
+      *streamables,
+      content: turbo_stream_action_tag(action, **opts)
+    )
+  end
+
+  def broadcast_dispatch_message_later_to(*streamables, action:, **opts)
+    Turbo::Streams::BroadcastStreamJob.perform_later(
+      stream_name_from(streamables),
+      content: turbo_stream_action_tag(action, **opts)
+    )
+  end
+end
+
+module Turbo::Broadcastable
+  def broadcast_dispatch_event_to(*streamables, event:, data: {})
+    broadcast_dispatch_message(*streamables, action: :dispatch_event, event:, data:)
+  end
+
+  def broadcast_dispatch_event_later_to(*streamables, event:, data: {})
+    broadcast_dispatch_message_later(*streamables, action: :dispatch_event, event:, data:)
+  end
+
+  private
+  def broadcast_dispatch_message(*streamables, **opts)
+    unless suppressed_turbo_broadcasts?
+      Turbo::StreamsChannel.broadcast_dispatch_message_to(*streamables, **opts)
+    end
+  end
+
+  def broadcast_dispatch_message_later(*streamables, **opts)
+    unless suppressed_turbo_broadcasts?
+      Turbo::StreamsChannel.broadcast_dispatch_message_later_to(*streamables, **opts)
+    end
+  end
+end
+
 Turbo::Streams::TagBuilder.prepend(TurboStreamDispatchEventHelper)
