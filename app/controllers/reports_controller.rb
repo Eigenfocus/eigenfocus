a14e29c4de63d312db481ffcd06e5b57049cc0cf
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-21 19:17:43 -0300

    chore: adds CsvDownloadable controller concern

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,6 @@
 class ReportsController < ApplicationController
+  include CsvDownloadable
+
   def total_time
     @q = TimeEntry.ransack(params[:q])
 
@@ -6,24 +8,18 @@ class ReportsController < ApplicationController
       @time_entries = @q.result
                       .includes(:project, :issue)
                     .order(reference_date: :asc)
-
       @total_in_hours = (@time_entries.sum(:total_logged_time_in_minutes)/60.0).round(2)
     else
       @time_entries = TimeEntry.none
       @total_in_hours = 0
     end
 
-
     respond_to do |format|
-      format.csv do
-        # Encoding is set to iso-8859-1 to avoid issues with special characters
-        # Especially on Windows and Excel.
-        send_data(TotalTimeLoggedCsvReport.new(@time_entries, @total_in_hours).generate_csv.encode("iso-8859-1"),
-        type: "text/csv; charset=iso-8859-1; header=present",
-        filename: "total-time-#{DateTime.current.strftime('%Y-%M-%d-%H-%M')}.csv")
-      end
-
       format.html
+      format.csv do
+        report = TotalTimeLoggedCsvReport.new(@time_entries, @total_in_hours)
+        send_csv_response(report.generate_csv, "total-time")
+      end
     end
   end
 end
