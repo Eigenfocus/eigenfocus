789f70a23b4d9eb0b8afb2abe32f05b82d39181d
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-23 16:24:30 -0300

    Binding Crepe editor with issue description field

--- frontend/components/MarkdownEditor.js
+++ frontend/components/MarkdownEditor.js
@@ -1,14 +1,24 @@
 import React from "react"
 
 import { Milkdown, MilkdownProvider, useEditor } from "@milkdown/react"
+import { Editor, rootCtx, defaultValueCtx } from "@milkdown/kit/core"
 import { Crepe } from "@milkdown/crepe";
 
 function CrepeEditor(props) {
   useEditor((root) => {
-    return new Crepe({
+    const crepe = new Crepe({
       root,
       ...props,
     })
+
+    crepe.on(listener => {
+      listener. markdownUpdated((_, markdown, prevMarkdown) => {
+        const target = document.querySelector(props.bindTarget)
+        target.value = markdown
+      });
+    });
+
+    return crepe
   }, [])
 
   return (
