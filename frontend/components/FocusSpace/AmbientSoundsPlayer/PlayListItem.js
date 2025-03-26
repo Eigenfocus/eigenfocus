9951b3a93a25a531b098ce163e98e31647c134f7
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-26 14:56:06 -0300

    Fixes ambient sounds to use stored volume confige

--- frontend/components/FocusSpace/AmbientSoundsPlayer/PlayListItem.js
+++ frontend/components/FocusSpace/AmbientSoundsPlayer/PlayListItem.js
@@ -8,7 +8,7 @@ const PlayListItem = ({
   title,
   icon,
   isSelected,
-  volume = 0.5,
+  volume,
   isPlaying,
   onSelect,
   onDeselect,
@@ -21,6 +21,7 @@ const PlayListItem = ({
       const audio = new Audio(src)
       audioRef.current = audio
       audio.loop = true
+      audioRef.current.volume = volume
     }
     if (isPlaying) {
       audioRef.current.play()
