21600a82dabd9bfc651fdade24921bea3050ecbc
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-27 00:51:14 -0300

    ref: Moves ambient sound sound icons to component folder

--- frontend/components/FocusSpace/AmbientSoundsPlayer/sounds.js
+++ frontend/components/FocusSpace/AmbientSoundsPlayer/sounds.js
@@ -10,42 +10,42 @@ const sounds = {
   rain: {
     src: soundUrl('rain'),
     title: t('focus_space.ambient_sounds.rain'),
-    icon: '/sound-icons/rain.svg'
+    icon: require('./sound-icons/rain.svg')
   },
   thunder: {
     src: soundUrl('thunder'),
     title: t('focus_space.ambient_sounds.thunder'),
-    icon: '/sound-icons/thunder.svg'
+    icon: require('./sound-icons/thunder.svg')
   },
   birds: {
     src: soundUrl('birds'),
     title: t('focus_space.ambient_sounds.birds'),
-    icon: '/sound-icons/birds.svg'
+    icon: require('./sound-icons/birds.svg')
   },
   fire: {
     src: soundUrl('fire'),
     title: t('focus_space.ambient_sounds.fire'),
-    icon: '/sound-icons/fire.svg'
+    icon: require('./sound-icons/fire.svg')
   },
   office: {
     src: soundUrl('office'),
     title: t('focus_space.ambient_sounds.office'),
-    icon: '/sound-icons/office.svg'
+    icon: require('./sound-icons/office.svg')
   },
   forest: {
     src: soundUrl('forest'),
     title: t('focus_space.ambient_sounds.forest'),
-    icon: '/sound-icons/forest.svg'
+    icon: require('./sound-icons/forest.svg')
   },
   walk: {
     src: soundUrl('walk'),
     title: t('focus_space.ambient_sounds.walk'),
-    icon: '/sound-icons/walk.svg'
+    icon: require('./sound-icons/walk.svg')
   },
   whitenoise: {
     src: soundUrl('whitenoise'),
     title: t('focus_space.ambient_sounds.whitenoise'),
-    icon: '/sound-icons/noise.svg'
+    icon: require('./sound-icons/noise.svg')
   }
 }
 
