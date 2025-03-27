e52741c2c94120971f593888ac8de06e393ea32b
Vinícius Oyama <contact@viniciusoyama.com>
2025-03-26 21:43:07 -0300

    Formats start/stop pomodoro button

--- frontend/components/FocusSpace/PomodoroTimer/TimerControls.js
+++ frontend/components/FocusSpace/PomodoroTimer/TimerControls.js
@@ -1,18 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const TimerControls = ({ isActive, onStartPause, onReset }) => {
+import { faClockRotateLeft, faClock, faPause } from '@fortawesome/free-solid-svg-icons'
+
+const TimerControls = ({ isRunning, onStartPause, onReset }) => {
   return (
     <div className="timer-controls">
       <button
-        className="button-primary big"
+        className="button-primary big start-pause-button"
         onClick={onStartPause}
       >
-        {isActive ? "Pause" : "Start"}
+        {isRunning ?
+          (<FontAwesomeIcon icon={faPause} />) :
+          (<FontAwesomeIcon icon={faClock} />)}
+
+        {isRunning ? "Pause" : "Start"}
       </button>
       <button
         className="button-primary big"
         onClick={onReset}
       >
+        <FontAwesomeIcon icon={faClockRotateLeft} />
         Reset
       </button>
     </div>
