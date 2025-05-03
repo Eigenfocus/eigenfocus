e8cad4ab5ab34521603e2801c5d15d1fb8a1c85a
Vinícius Oyama <contact@viniciusoyama.com>
2025-05-03 20:18:34 -0300

    Adds real time updates to comments

--- app/models/issue/comment.rb
+++ app/models/issue/comment.rb
@@ -1,5 +1,31 @@
 class Issue::Comment < ApplicationRecord
+  # Associations
   belongs_to :issue
 
+  # Validations
   validates :content, presence: true
+
+  # Broadcasts
+  after_create_commit -> {
+    broadcast_append_later_to(
+      "issue_#{issue.id}/comments",
+      partial: "issues/comments/comment",
+      locals: {
+        comment: self
+      },
+      target: "issue-comments-list"
+    )
+  }
+
+  after_update_commit -> {
+    broadcast_replace_later_to(
+      "issue_#{issue.id}/comments",
+      partial: "issues/comments/comment",
+      locals: {
+        comment: self
+      }
+    )
+  }
+
+  after_destroy_commit -> { broadcast_remove_to "issue_#{issue.id}/comments" }
 end
