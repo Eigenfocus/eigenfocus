a43e0c0538ad6c950e5d6e4a565be1b0a00c38bc
Vinícius Oyama <contact@viniciusoyama.com>
2025-05-04 14:33:16 -0300

    chore: Adds click to edit behaviour to issue description

--- frontend/components/IssueDetail/Description.js
+++ frontend/components/IssueDetail/Description.js
@@ -1,14 +1,43 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 import MarkdownEditor from "../MarkdownEditor"
+import { t } from "i18n.js.erb"
 
 const Description = ({ content, issueId }) => {
   const hiddenFieldRef = useRef(null)
+  const [isEditing, setIsEditing] = useState(false)
 
   return (
-    <React.Fragment>
-      <MarkdownEditor defaultValue={content} readOnly={false} mirrorInputTargetRef={hiddenFieldRef}/>
-      <input type="hidden" name="issue[description]" value={content ? content : ""} ref={hiddenFieldRef}/>
-    </React.Fragment>
+    <div>
+      <div class="mt-2 mb-2 flex items-center justify-between">
+        <h3 class="text-lg font-medium text-readable-content-500">
+          <span class="mr-1 text-readable-content-500/80">
+            <i class="fa-solid text-sm fa-align-justify"></i>
+          </span>
+          { t("activerecord.attributes.issue.description") }
+        </h3>
+
+        { !isEditing && (
+          <a className="btn-primary text-sm cursor-pointer btn-sm" onClick={() => { setIsEditing(true) }}>
+            { t("actions.edit") }
+          </a>
+        )}
+      </div>
+      <div onClick={() => { setIsEditing(true) }}>
+        <MarkdownEditor defaultValue={content} readOnly={!isEditing} mirrorInputTargetRef={hiddenFieldRef}/>
+        <input type="hidden" name="issue[description]" value={content ? content : ""} ref={hiddenFieldRef}/>
+      </div>
+      { isEditing && (
+        <div className="flex gap-4 items-center mt-2 justify-end">
+          <a className="link-cancel text-sm" onClick={() => { setIsEditing(false) }}>
+            { t("actions.cancel") }
+          </a>
+
+          <button className="btn-primary">
+            { t("actions.save") }
+          </button>
+        </div>
+      )}
+    </div>
   )
 }
 
