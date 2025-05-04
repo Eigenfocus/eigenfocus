7a8c48ccbd8726f6a191ebee589034c4e9e88c81
Vinícius Oyama <contact@viniciusoyama.com>
2025-05-04 10:40:10 -0300

    Fixes interement failing spec

--- app/controllers/issues/comments_controller.rb
+++ app/controllers/issues/comments_controller.rb
@@ -1,37 +1,34 @@
+class Issues::CommentsController < ApplicationController
+  def create
+    @comment = current_issue.comments.new(comment_params)
+    @comment.author = current_user
+    @comment.save
+  end
 
-module Issues
-  class CommentsController < ApplicationController
-    def create
-      @comment = current_issue.comments.new(comment_params)
-      @comment.author = current_user
-      @comment.save
+  def edit
+    @comment = current_issue.comments.find(params[:id])
+  end
+
+  def update
+    @comment = current_issue.comments.find(params[:id])
+
+    unless params[:commit] == "cancel"
+      @comment.update(comment_params)
     end
+  end
 
-    def edit
-      @comment = current_issue.comments.find(params[:id])
-    end
+  def destroy
+    @comment = current_issue.comments.find(params[:id])
+    @comment.destroy
+  end
 
-    def update
-      @comment = current_issue.comments.find(params[:id])
+  private
 
-      unless params[:commit] == "cancel"
-        @comment.update(comment_params)
-      end
-    end
+  def current_issue
+    @current_issue ||= Issue.find(params[:issue_id])
+  end
 
-    def destroy
-      @comment = current_issue.comments.find(params[:id])
-      @comment.destroy
-    end
-
-    private
-
-    def current_issue
-      @current_issue ||= Issue.find(params[:issue_id])
-    end
-
-    def comment_params
-      params.require(:issue_comment).permit(:content)
-    end
+  def comment_params
+    params.require(:issue_comment).permit(:content)
   end
 end
