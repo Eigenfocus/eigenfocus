fc67dc20c95cceb13146e7479e53f0b7b4f42809
Jun <felipe.jun.ohira@gmail.com>
2025-05-29 11:39:36 -0300

    Rename helper

--- app/helpers/application_helper/issues.rb
+++ app/helpers/application_helper/issues.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   module Issues
-    def badge_for_issue_id(issue)
+    def global_issue_link(issue)
       wrapper_options = {
         class: "popover",
         data: {
           controller: "copy popover replace animation",
           clipboard_text: project_item_url(issue.project, issue.id),
           action: "copy:success->replace#innerText copy:success->animation#pulse mouseover->popover#mouseOver mouseout->popover#mouseOut",
-          replace_value_param: t("helpers.issues.badge_for_issue_id.copied"),
+          replace_value_param: t("helpers.issues.global_issue_link.copied"),
           replace_reset_timeout_param: 1000,
           popover_translate_x: "0",
           popover_translate_y: "-50%",
@@ -20,7 +20,7 @@ module ApplicationHelper
         concat content_tag(:span, "##{issue.id}", class: "inline-block text-sm cursor-pointer link-primary js-animation-target-#{issue.id}")
         concat(
           content_tag(:div, class: "popover__content opacity-0 top-1/2 left-full ml-2 text-nowrap", data: { popover_target: "content" }) do
-            content_tag(:span, t("helpers.issues.badge_for_issue_id.click_to_copy_url"), data: { replace_target: "content" })
+            content_tag(:span, t("helpers.issues.global_issue_link.click_to_copy_url"), data: { replace_target: "content" })
           end
         )
       end
