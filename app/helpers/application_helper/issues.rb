3aba96a000172a85cc78b5d966476463a655ed11
Jun <felipe.jun.ohira@gmail.com>
2025-05-29 10:31:19 -0300

    Fix popover align with issue ID

--- app/helpers/application_helper/issues.rb
+++ app/helpers/application_helper/issues.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
           replace_value_param: t("helpers.issues.badge_for_issue_id.copied"),
           replace_reset_timeout_param: 1000,
           popover_translate_x: "0",
-          popover_translate_y: "-25%",
+          popover_translate_y: "-50%",
           animation_target_param: ".js-animation-target-#{issue.id}"
         }
       }
@@ -18,7 +18,7 @@ module ApplicationHelper
       content_tag(:span, wrapper_options) do
         concat content_tag(:span, "##{issue.id}", class: "inline-block text-sm cursor-pointer link-primary js-animation-target-#{issue.id}")
         concat(
-          content_tag(:div, class: "content hidden top-0 left-full ml-2 text-nowrap", data: { popover_target: "content" }) do
+          content_tag(:div, class: "popover__content hidden top-1/2 left-full ml-2 text-nowrap", data: { popover_target: "content" }) do
             content_tag(:span, t("helpers.issues.badge_for_issue_id.click_to_copy_url"), data: { replace_target: "content" })
           end
         )
