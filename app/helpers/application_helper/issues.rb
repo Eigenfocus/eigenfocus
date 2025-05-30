2b0139ff246438b2a98709bae098e5f4a2eb4e8e
Jun <felipe.jun.ohira@gmail.com>
2025-05-30 15:45:50 -0300

    Change animation controller target to use stimulus controller targets

--- app/helpers/application_helper/issues.rb
+++ app/helpers/application_helper/issues.rb
@@ -9,13 +9,12 @@ module ApplicationHelper
           action: "copy:success->issue--global-link#onCopy copy:success->animation#pulse mouseover->issue--global-link#mouseOver mouseout->issue--global-link#mouseOut",
           issue__global_link_initial_text_value: t("helpers.issues.global_issue_link.click_to_copy_url"),
           issue__global_link_on_copy_text_value: t("helpers.issues.global_issue_link.copied"),
-          animation_target_param: ".js-animation-target-#{issue.id}",
           animation_speed_value: "fast"
         }
       }
 
       content_tag(:span, wrapper_options) do
-        concat content_tag(:span, "##{issue.id}", class: "inline-block text-sm cursor-pointer link-primary js-animation-target-#{issue.id}")
+        concat content_tag(:span, "##{issue.id}", class: "inline-block text-sm cursor-pointer link-primary js-animation-target-#{issue.id}", data: { animation_target: "animatable" })
         concat(
           content_tag(:div, nil, class: "popover__content opacity-0 top-1/2 left-full -translate-y-1/2 ml-2 text-nowrap", data: { issue__global_link_target: "content" })
         )
