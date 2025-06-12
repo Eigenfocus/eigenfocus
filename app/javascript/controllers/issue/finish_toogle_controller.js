1dc7503296a0a42dc8f0d9ad7da68cc7110de4cb
Vinícius Oyama <contact@viniciusoyama.com>
2025-06-12 11:23:46 -0300

    ref: issue finish toogle to use a single div with no helper

---
+++ app/javascript/controllers/issue/finish_toogle_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+import { FetchRequest } from '@rails/request.js'
+
+export default class extends Controller {
+  static values = {
+    finishPath: { type: String },
+    unfinishPath: { type: String },
+  }
+
+  toogle(e) {
+    e.preventDefault()
+    if (this.element.classList.contains("finished")) {
+      this.markAsUnchecked()
+    } else {
+      this.markAsChecked()
+    }
+  }
+
+  markAsChecked() {
+    this.element.classList.add("finished")
+    this.dispatch("checked")
+    this.#request(this.finishPathValue)
+  }
+
+  markAsUnchecked() {
+    this.element.classList.remove("finished")
+    this.dispatch("unchecked")
+    this.#request(this.unfinishPathValue)
+  }
+
+  #request(path) {
+    const request = new FetchRequest('put', path)
+
+    return request.perform()
+  }
+}
