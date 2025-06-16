853db13112de6a9fb38ddafe73adf9e12b5aa700
Vinícius Oyama <contact@viniciusoyama.com>
2025-06-16 15:20:38 -0300

    Updates issue list when finished_at is changed

--- spec/features/issues/mark_as_finished_spec.rb
+++ spec/features/issues/mark_as_finished_spec.rb
@@ -7,7 +7,7 @@ describe "Issues - Mark as finished" do
   context 'When issue is not finished yet' do
     let!(:issue) { create(:issue, :unfinished, project:) }
 
-    specify "I can mark issue as finished directly on board view" do
+    specify "I can mark issue as finished" do
       visit project_show_issue_path(project, issue)
 
       expect(page).to have_content(issue.title)
@@ -15,6 +15,11 @@ describe "Issues - Mark as finished" do
       find(".cpy-finish-check-toogle").click
 
       expect(page).to have_css(".cpy-finish-check-toogle.finished")
+
+      within("#issue_#{issue.id}") do
+        expect(page).to have_css(".fa-check")
+      end
+
       expect(issue.reload).to be_finished
     end
   end
@@ -22,13 +27,17 @@ describe "Issues - Mark as finished" do
   context 'When issue is not finished yet' do
     let!(:issue) { create(:issue, :finished, project:) }
 
-    specify "I can mark issue as unfinished directly on board view" do
+    specify "I can mark issue as unfinished" do
       visit project_show_issue_path(project, issue)
 
       find(".cpy-finish-check-toogle").click
 
       expect(page).to_not have_css(".cpy-finish-check-toogle.finished")
       expect(issue.reload).to_not be_finished
+
+      within("#issue_#{issue.id}") do
+        expect(page).to_not have_css(".fa-check")
+      end
     end
   end
 end
