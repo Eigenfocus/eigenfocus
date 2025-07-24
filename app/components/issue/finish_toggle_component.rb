c20305e5eccda0a9d267699d79aa103ef2d1da84
Vinícius Oyama <contact@viniciusoyama.com>
2025-07-23 22:07:34 -0300

    Adds issue finish toggle to list view

--- app/components/issue/finish_toggle_component.rb
+++ app/components/issue/finish_toggle_component.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class Issue::FinishToggleComponent < ViewComponent::Base
-  def initialize(issue:)
+  def initialize(issue:, minimal_style: false)
     @issue = issue
+    @minimal_style = minimal_style
+  end
+
+  def style_classes
+    classes = []
+    classes << "minimal" if @minimal_style
+    classes << "finished" if @issue.finished?
+    classes.join(" ")
   end
 end
