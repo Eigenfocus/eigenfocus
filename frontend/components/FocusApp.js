f142b7c80141f75f69fcc02596fed48c9732843c
Vinícius Oyama <contact@viniciusoyama.com>
2025-10-17 23:18:25 -0300

    Moves focus space button to header

--- frontend/components/FocusApp.js
+++ frontend/components/FocusApp.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark, faWindowRestore, faVolumeHigh, faHourglassHalf } from '@fortawesome/free-solid-svg-icons'
+import { IconFocus, IconHourglassEmpty, IconBrandDeezer } from '@tabler/icons-react';
 
 import AnimatedBackground from "./FocusApp/AnimatedBackground"
 import FocusSpace from "./FocusApp/FocusSpace"
@@ -17,6 +18,8 @@ const FocusApp = ({ }) => {
     setPomodoroState(newState)
   }
 
+  const showPomodoroIcon = (pomodoroState === POMODORO_STATE.RUNNING || pomodoroState === POMODORO_STATE.PAUSED)
+
   return (
     <>
       {isFocusSpaceShowing && (
@@ -30,31 +33,33 @@ const FocusApp = ({ }) => {
           onPomodoroStateChange={handlePomodoroStateChange}
           onHide={() => setIsFocusSpaceShowing(false)} />
       </div>
-      <div className={`focus-space-access-buttons ${isFocusSpaceShowing ? 'space-showing' : ''}`}>
-        <button className={`tour--open-focus-app-button open-space-button ${isFocusSpaceShowing ? 'close' : 'open'}`} onClick={() => setIsFocusSpaceShowing(!isFocusSpaceShowing)}>
-          <FontAwesomeIcon icon={isFocusSpaceShowing ? faXmark : faWindowRestore} />
+      <div className={`${isFocusSpaceShowing ? 'space-showing' : ''}`}>
+        <button className={`tour--open-focus-app-button btn btn-sm px-1.5 btn-ghost ${isFocusSpaceShowing ? 'close' : 'open'}`} onClick={() => setIsFocusSpaceShowing(!isFocusSpaceShowing)}>
+          {
+            !hasSoundPlaying &&
+            !showPomodoroIcon &&
+            (<IconFocus/>)
+          }
           {!isFocusSpaceShowing && (
             <>
               {(pomodoroState === POMODORO_STATE.FINISHED) && (
-                <span className="ping-notification">
-                  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75"></span>
-                  <span className="relative inline-flex size-3 rounded-full bg-primary"></span>
-                </span>
+                <div aria-label="status" class="status status-info animate-bounce"></div>
               )}
 
               {hasSoundPlaying && (
                 <span className='sound-playing-icon'>
-                  <FontAwesomeIcon icon={faVolumeHigh} />
+                  <IconBrandDeezer />
                 </span>
               )}
 
-              {(pomodoroState === POMODORO_STATE.RUNNING || pomodoroState === POMODORO_STATE.PAUSED) && (
+              {showPomodoroIcon && (
                 <span className='pomodoro-running-icon'>
-                  <FontAwesomeIcon icon={faHourglassHalf} />
+                  <IconHourglassEmpty />
                 </span>
               )}
             </>
           )}
+          Focus space
         </button>
       </div>
     </>
