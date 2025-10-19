e6da9c5505e97ae8e00715987017e71f6d2f353f
Vinicius Oyama <contact@viniciusoyama.com>
2025-10-19 13:23:51 -0300

    Update project form to new modal

--- app/components/modal/dialog_component.rb
+++ app/components/modal/dialog_component.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
 class Modal::DialogComponent < ViewComponent::Base
-  attr_reader :title, :icon
+  attr_reader :title, :icon, :dialog_options
 
-  def initialize(title: nil, icon: nil)
+  def initialize(title: nil, icon: nil, dialog_options: {})
     @title = title
     @icon = icon
+    @dialog_options = dialog_options
   end
 
   def render_title
     return if title.blank?
     render Modal::TitleComponent.new(title: title, icon: icon)
+  end
+
+  def dialog_classes
+    dialog_options[:class].to_s
   end
 end
