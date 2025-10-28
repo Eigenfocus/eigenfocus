f00dc7351bff8ffc4daf69f601f6bf4734568d86
Vinícius Oyama <contact@viniciusoyama.com>
2025-10-28 09:38:11 -0300

    Ajust CSV dowloader encoding

--- app/controllers/concerns/csv_downloadable.rb
+++ app/controllers/concerns/csv_downloadable.rb
@@ -5,7 +5,7 @@ module CsvDownloadable
     # Encoding is set to iso-8859-1 to avoid problems with special characters.
     # Especially on Windows and Excel.
     send_data(
-      data.encode("iso-8859-1"),
+      data.force_encoding("iso-8859-1"),
       type: "text/csv; charset=iso-8859-1; header=present",
       filename: "#{filename}-#{DateTime.current.strftime('%Y-%M-%d-%H-%M')}.csv"
     )
