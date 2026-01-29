b2ecb3a39932b983fa644c2e2a3f0971f40b9d17
Vinícius Oyama <contact@viniciusoyama.com>
2026-01-29 13:48:15 -0300

    Change default path for running time entries notification link

--- app/components/header/running_time_entries_component.rb
+++ app/components/header/running_time_entries_component.rb
@@ -8,4 +8,9 @@ class Header::RunningTimeEntriesComponent < ViewComponent::Base
   def show_button?
     count > 0
   end
+
+  def oldest_active_timesheet_day_path
+    reference_date = helpers.current_user.running_time_entries.order(reference_date: :asc).pick(:reference_date)
+    time_entries_path(reference_date: reference_date.strftime("%Y-%m-%d"))
+  end
 end
