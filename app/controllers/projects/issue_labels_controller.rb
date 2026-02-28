f00d4728aa3c7676fabc5cf4401cdf074bb9c539
Vinícius Oyama <contact@viniciusoyama.com>
2026-02-28 10:09:10 -0300

    Add JSON response to IssueLabelsController#index
    Return all labels as JSON (id + title) without pagination, to support
    AJAX loading from the SearchableSelect component.

--- app/controllers/projects/issue_labels_controller.rb
+++ app/controllers/projects/issue_labels_controller.rb
@@ -3,9 +3,15 @@ class Projects::IssueLabelsController < Projects::BaseController
     @q = current_project.issue_labels.ransack(params[:q])
     @q.sorts = "updated_at desc" if @q.sorts.empty?
 
-    skip_layout_content_wrapper!
-
-    @pagy, @issue_labels = pagy(@q.result)
+    respond_to do |format|
+      format.html do
+        skip_layout_content_wrapper!
+        @pagy, @issue_labels = pagy(@q.result)
+      end
+      format.json do
+        render json: current_project.issue_labels.map { |l| { id: l.id, title: l.title } }
+      end
+    end
   end
 
   def new
