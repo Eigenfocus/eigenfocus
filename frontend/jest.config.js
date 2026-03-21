7c35eecab4beff81af518ea0a60a59d490527e24
Vinícius Oyama <contact@viniciusoyama.com>
2026-03-21 17:37:29 -0300

    Configure Jest to use SWC for transformation
    Replace the default Babel transform with @swc/jest so tests
    can parse ESM imports and JSX after the Babel-to-SWC migration.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   rootDir: '..',
-  setupFilesAfterEnv : ["<rootDir>/frontend/jest/react.js"]
+  setupFilesAfterEnv : ["<rootDir>/frontend/jest/react.js"],
+  transform: {
+    "^.+\\.jsx?$": ["@swc/jest", {
+      jsc: {
+        parser: {
+          jsx: true
+        }
+      }
+    }]
+  }
 }
