857a1470886fd86f89357514186d9f4c7de1688e
Vinícius Oyama <contact@viniciusoyama.com>
2026-03-22 01:11:45 -0300

    Remove flatpickr JS, CSS and locale entries
    Delete vendored flatpickr and stimulus-flatpickr JS files, custom
    flatpickr CSS styles, and flatpickr date format entries from locale
    files. Remove the CustomFlatpickr Stimulus controller from
    application.js and unpin from importmap.

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,8 +11,6 @@ pin "@rails/activestorage", to: "activestorage.esm.js"
 pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.5.2
 pin_all_from "vendor/javascript/tailwindcss-stimulus-components", under: "tailwindcss-stimulus-components", preload: true
 pin "moment" # @2.29.4
-pin "flatpickr" # @4.6.13
-pin "stimulus-flatpickr" # @3.0.0
 pin "select2" # @4.1.0
 pin "jquery" # @3.7.1
 pin "sortablejs" # @1.15.6
