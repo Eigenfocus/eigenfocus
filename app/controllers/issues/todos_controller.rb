6a93ff60f43c28a2959e15febe8e8f043a52f063
Vinícius Oyama <contact@viniciusoyama.com>
2026-06-22 22:40:37 -0300

    feat: add todos with inline editing and toggle

---
+++ app/controllers/issues/todos_controller.rb
@@ -0,0 +1,48 @@
+class Issues::TodosController < ApplicationController
+  def create
+    @todo = current_todo_list.todos.create
+  end
+
+  def edit
+    @todo = current_todo_list.todos.find(params[:id])
+  end
+
+  def update
+    @todo = current_todo_list.todos.find(params[:id])
+
+    if todo_params[:description].blank?
+      @todo.destroy
+    else
+      @todo.update(todo_params)
+    end
+  end
+
+  def destroy
+    @todo = current_todo_list.todos.find(params[:id])
+    @todo.destroy
+  end
+
+  def toggle
+    @todo = current_todo_list.todos.find(params[:id])
+
+    if @todo.finished?
+      @todo.unfinish!
+    else
+      @todo.finish!(current_user)
+    end
+  end
+
+  private
+
+  def current_issue
+    @current_issue ||= Issue.find(params[:issue_id])
+  end
+
+  def current_todo_list
+    @current_todo_list ||= current_issue.todo_lists.find(params[:todo_list_id])
+  end
+
+  def todo_params
+    params.require(:issue_todo).permit(:description)
+  end
+end
